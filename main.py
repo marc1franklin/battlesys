f9c6db825f0942b871335351a30f4cf89d075118
Marc Franklin <marc1franklin@yahoo.com>
2020-03-02 12:14:17 -0600

    more pokemon added. added levels to each pokemon.

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import time
 import numpy as np 
 import sys 
 
-
+string_1_attack = "\nIts not very effective..."
+string_2_attack = "\nIts super effective!"
 
 #function that prints text slower
 def delay_print(s):
@@ -14,14 +15,15 @@ def delay_print(s):
 
 #create class
 class Pokemon:
-    def __init__(self, name, types, moves, EVs, health ='==================='):
+    def __init__(self, name, level, types, moves, EVs, health ='++++++++++++++++++++++++++++++++++++++++++++++++++'):
         self.name = name
+        self.level = level
         self.types = types
         self.moves = moves
         self.attack = EVs['ATTACK']
         self.defense = EVs['DEFENSE']
         self.health = health
-        self.bars = 20
+        self.bars = 50
     
     def fight(self, Pokemon2):
 
@@ -30,26 +32,26 @@ class Pokemon:
         print("TYPE/", self.types)
         print("ATTACK/", self.attack)
         print("DEFENSE/", self.defense)
-        print("LVL/", 3*(1+np.mean([self.attack,self.defense])))
+        print("LVL/", self.level)
 
         print("\nVS")
 
         print(f"\n{Pokemon2.name}")
-        print(f"\n{self.name}")
         print("TYPE/", Pokemon2.types)
         print("ATTACK/", Pokemon2.attack)
         print("DEFENSE/", Pokemon2.defense)
-        print("LVL/", 3*(1+np.mean([Pokemon2.attack,Pokemon2.defense])))
+        print("LVL/", self.level)
+
 
         time.sleep(2)
         #type advantages
-        version = ["Fire", "Water", "Grass"]
+        version = ["Fire", "Water", "Grass", "Electric", "Ice"]
         for i,k in enumerate(version):
             if self.types == k:
                 #both types are the same
                 if Pokemon2.types == k:
-                    string_1_attack = "\nIts not very effective..."
-                    string_2_attack = "\nIts not very effective..."
+                   print(string_1_attack)
+                   print(string_2_attack)
 
                 #Pokemon2 has type advantge
                 if Pokemon2.types == version[(i+1)%3]:
@@ -57,8 +59,8 @@ class Pokemon:
                     Pokemon2.defense *= 2
                     self.attack /= 2
                     self.defense /= 2
-                    string_1_attack = "\nIts not very effective..."
-                    string_2_attack = "\nIts super effective!"
+                    print(string_1_attack)
+                    print(string_2_attack)
 
                 # Pokemon 2 has type disadvantage
                 if Pokemon2.types == version[(i+2)%3]:
@@ -66,16 +68,16 @@ class Pokemon:
                     self.defense *= 2
                     Pokemon2.attack /= 2
                     Pokemon2.defense /= 2
-                    string_1_attack = "\nIts super effective!"
-                    string_2_attack = "\nIts not very effective..."
+                    print(string_1_attack)
+                    print(string_2_attack)
 
 
            # fighting / battling
         # continues while pokemon still have health            
         while (self.bars > 0) and (Pokemon2.bars > 0):
             #print health
-            print(f"\n{self.name}\t\tHLTH\t{self.health}")
-            print(f"{Pokemon2.name}\t\tHLTH\t{Pokemon2.health}\n")
+            print(f"\n{self.name}\t\tHLTH\t{self.health}\n")
+            print(f"\n{Pokemon2.name}\t\tHLTH\t{Pokemon2.health}\n")
 
             print(f"Go {self.name}!")
             for i,x in enumerate(self.moves):
@@ -91,7 +93,7 @@ class Pokemon:
 
             # Add back bars plus defense boost
             for i in range(int(Pokemon2.bars+.1*Pokemon2.defense)):
-                Pokemon2.health += "="
+                Pokemon2.health += "+"
 
             time.sleep(1)
             print(f"\n{self.name}\t\tHLTH\t{self.health}")
@@ -119,7 +121,7 @@ class Pokemon:
 
             # Add back bars plus defense boost
             for i in range(int(self.bars+.1*self.defense)):
-                Pokemon2.health += "="
+                Pokemon2.health += "+"
 
             time.sleep(1)
             print(f"{self.name}\t\tHLTH\t{self.health}")
@@ -142,20 +144,32 @@ class Pokemon:
 
 if __name__ == '__main__':
     #Create Pokemon
-    Charizard = Pokemon('Charizard', 'Fire', ['Flamethrower', 'Fly', 'Blast Burn', 'Fire Punch'], {'ATTACK':12, 'DEFENSE': 8})
-    Blastoise = Pokemon('Blastoise', 'Water', ['Water Gun', 'Bubblebeam', 'Hydro Pump', 'Surf'],{'ATTACK': 10, 'DEFENSE':10})
-    Venusaur = Pokemon('Venusaur', 'Grass', ['Vine Wip', 'Razor Leaf', 'Earthquake', 'Frenzy Plant'],{'ATTACK':8, 'DEFENSE':12})
+    Articuno = Pokemon('Articuno',{"LEVEL":70}, 'Ice', ['Ice Beam', 'Blizzard', 'Fly', 'Frost Breath'],{'ATTACK':20, 'DEFENSE':24})
+    Zapdos = Pokemon('Zapdos',{"LEVEL":70}, 'Electric', ['Thunder', 'Thunderbolt', 'Fly', 'Drill Peck'],{'ATTACK':16, 'DEFENSE':28})
+    Moltres = Pokemon('Moltres',{"LEVEL":70}, 'Fire',  ["Flamethrower", "Overheat", "Fly", "Hyper Beam"],{"ATTACK":18, "DEFENSE":26})
+ 
+    Charizard = Pokemon('Charizard',{"LEVEL":50}, 'Fire', ['Flamethrower', 'Fly', 'Blast Burn', 'Fire Punch'], {'ATTACK':12, 'DEFENSE': 8})
+    Blastoise = Pokemon('Blastoise',{"LEVEL":50}, 'Water', ['Water Gun', 'Bubblebeam', 'Hydro Pump', 'Surf'],{'ATTACK': 10, 'DEFENSE':10})
+    Venusaur = Pokemon('Venusaur',{"LEVEL":50}, 'Grass', ['Vine Wip', 'Razor Leaf', 'Earthquake', 'Frenzy Plant'],{'ATTACK':8, 'DEFENSE':12})
 
-    Charmander = Pokemon('Charmander', 'Fire', ['Ember', 'Scratch', 'Tackle', 'Fire Punch'],{'ATTACK':4, 'DEFENSE':2})
-    Squirtle = Pokemon('Squirtle', 'Water', ['Bubblebeam', 'Tackle', 'Headbutt', 'Surf'],{'ATTACK': 3, 'DEFENSE':3})
-    Bulbasaur = Pokemon('Bulbasaur', 'Grass', ['Vine Wip', 'Razor Leaf', 'Tackle', 'Leech Seed'],{'ATTACK':2, 'DEFENSE':4})
+    Charmander = Pokemon('Charmander',{"LEVEL":25}, 'Fire', ['Ember', 'Scratch', 'Tackle', 'Fire Punch'],{'ATTACK':4, 'DEFENSE':2})
+    Squirtle = Pokemon('Squirtle',{"LEVEL":25}, 'Water', ['Bubblebeam', 'Tackle', 'Headbutt', 'Surf'],{'ATTACK': 3, 'DEFENSE':3})
+    Bulbasaur = Pokemon('Bulbasaur',{"LEVEL":25}, 'Grass', ['Vine Wip', 'Razor Leaf', 'Tackle', 'Leech Seed'],{'ATTACK':2, 'DEFENSE':4})
 
-    Charmeleon = Pokemon('Charmeleon', 'Fire', ['Ember', 'Scratch', 'Flamethrower', 'Fire Punch'],{'ATTACK':6, 'DEFENSE':5})
-    Wartortle = Pokemon('Wartortle', 'Water', ['Bubblebeam', 'Water Gun', 'Headbutt', 'Surf'],{'ATTACK': 5, 'DEFENSE':5})
-    Ivysaur = Pokemon('Ivysaur\t', 'Grass', ['Vine Wip', 'Razor Leaf', 'Bullet Seed', 'Leech Seed'],{'ATTACK':4, 'DEFENSE':6})
+    Charmeleon = Pokemon('Charmeleon',{"LEVEL":35}, 'Fire', ['Ember', 'Scratch', 'Flamethrower', 'Fire Punch'],{'ATTACK':6, 'DEFENSE':5})
+    Wartortle = Pokemon('Wartortle',{"LEVEL":35}, 'Water', ['Bubblebeam', 'Water Gun', 'Headbutt', 'Surf'],{'ATTACK': 5, 'DEFENSE':5})
+    Ivysaur = Pokemon('Ivysaur\t',{"LEVEL":35}, 'Grass', ['Vine Wip', 'Razor Leaf', 'Bullet Seed', 'Leech Seed'],{'ATTACK':4, 'DEFENSE':6})
+    
+    #!!!!!!!!!!!!!
+    Wooper = Pokemon('Wooper',{"LEVEL":100}, 'Water',  ["Hydro Pump", "Aqua Tail", "Sheer Cold", "Earthquake"],{"ATTACK":999, "DEFENSE":999})
+    #!!!!!!!!!!!!
+ 
+ 
+
+
 
     #Makes them fight
-    Charizard.fight(Blastoise)
+    Articuno.fight(Zapdos)
 
 
 
